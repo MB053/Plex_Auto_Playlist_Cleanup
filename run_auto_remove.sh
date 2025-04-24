8aadee51ba3a8e0e298ebff3e28368fc5bfe1b7f
MB053 <56123651+MB053@users.noreply.github.com>
2025-04-24 12:37:00 +0200

    Update run_auto_remove.sh

--- run_auto_remove.sh
+++ run_auto_remove.sh
@@ -2,7 +2,7 @@
 
 # == INFORMATION ==
 # Make this executable by running chmod +x /home/USER_LOCATION/run_auto_remove.sh (Change USER_LOCATION to actual script location)
-
+# Please make sure to build the plex_env on the location 
 # === Absolute paths ===
 VENV_PATH="/home/USER_LOCATION/plex_env"   #Change USER_LOCATION to actual script location
 SCRIPT_PATH="/home/USER_LOCATION/Auto_Remove_Script.py"  #Change USER_LOCATION to actual script location
